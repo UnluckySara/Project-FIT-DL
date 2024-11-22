a9d4296bb20afc4587bdc6cba2188bd705906abb
jkozdroj <jakub.kozdroj@outlook.com>
2024-11-22 15:19:50 +0100

    Add files via upload

--- main.ino
+++ main.ino
@@ -3,10 +3,15 @@
 #include <Arduino_HTS221.h>
 #include "i2c.h"
 #include "i2c_LPS331.h"
+#include <TSL2571.h>
 
 
+TSL2571 tsl;
 LPS331 lps331;
-Generic_LM75 temperature;
+Generic_LM75 stlm75;
+
+
+
 bool start_screen = true;
 String inputBuffer = "";
 float upper_temp_value = 30.0;
@@ -14,7 +19,9 @@ float lower_temp_value = -5.0;
 float critical_pressure = 1000.0;
 float critical_humidity = 90.0;
 
+float pressure, temperature, humidity, illuminance = {};
 
+void(* resetFunc) (void) = 0;
 
 void setup()
 {
@@ -36,7 +43,10 @@ void setup()
       Serial.println("Sensor missing");
       while (1);
     }
+    tsl.getAddr_TSL2571(TSL2571_DEFAULT_ADDRESS);
+    tsl.begin();
 
+    delay(1000);
 }
  
 void loop()
@@ -47,13 +57,17 @@ void loop()
   }
   else
   {
-    float pressure, tempLPS;
-    float tempHTS = HTS.readTemperature();
-    float humidity = HTS.readHumidity();
-   
+    //lps331.getTemperature(tempLPS);
+    //float tempHTS = HTS.readTemperature();
+    humidity = HTS.readHumidity();
     lps331.getMeasurement(pressure);
-    lps331.getTemperature(tempLPS);
-    float tempSTLM = temperature.readTemperatureC();
+    temperature = stlm75.readTemperatureC();
+
+    tsl.setUpALS();
+    delay(500);
+    tsl.Measure_ALS();
+    illuminance = tsl.tsl_alsData.L;
+
   
     Serial.print("Humidity HTS221: ");
     Serial.print(humidity);
@@ -63,22 +77,26 @@ void loop()
     Serial.print(pressure);
     Serial.println(" mbar");
 
-    Serial.print("Temperature LPS331: ");
-    Serial.print(tempLPS);
-    Serial.println(" C");
-
     Serial.print("Temperature STLM75: ");
-    Serial.print(tempSTLM);
+    Serial.print(temperature);
     Serial.println(" C");
 
-    Serial.print("Temperature HTS221: ");
-    Serial.print(tempHTS);
-    Serial.println(" C");
-
-
+    Serial.print("Illuminance TSL2571: ");
+    Serial.print(illuminance);
+    Serial.println(" lux");
+    
     //Serial.print("Luminance TSL25721: ");
     //Serial.print(luminance);
     //Serial.println(" lux");
+    
+    //Serial.print("Temperature HTS221: ");
+    //Serial.print(tempHTS);
+    //Serial.println(" C");
+
+    //Serial.print("Temperature LPS331: ");
+    //Serial.print(tempLPS);
+    //Serial.println(" C");
+
 
     Serial.println();
     if (Serial.available() > 0)
@@ -86,7 +104,8 @@ void loop()
     start_screen = true;
     Serial.println("Press twice on the space.");
   }
-   delay(1000); 
+   delay(1000);
+   start_screen=true;
   }
   
     
@@ -98,38 +117,39 @@ void Title_Window()
 {
    if (Serial.available() > 0) 
     { 
+      
       String input = Serial.readStringUntil('\n'); 
-      if (input.equals(" "))
-      {       
-              Serial.println();
-              Serial.println("Welcome to the configuration window.");
-              Serial.println();
-              
-              Serial.println("Current settings:");
-              Serial.print("Critical value humidity: ");
-              Serial.print(critical_humidity);
-              Serial.println(" %");
-              Serial.print("Critical value pressure: ");
-              Serial.print(critical_pressure);
-              Serial.println(" mbar");
-              Serial.print("Lower critical temperature value: ");
-              Serial.print(lower_temp_value);
-              Serial.println(" C");
-              Serial.print("Upper critical temperature value: ");
-              Serial.print(upper_temp_value);
-              Serial.println(" C");
-              Serial.println();
+      if (input.equals("?"))
+      {    
+          Serial.println();
+          Serial.println("Welcome to the configuration window.");
+          Serial.println();
+          
+          Serial.println("Current settings:");
+          Serial.print("Critical value humidity: ");
+          Serial.print(critical_humidity);
+          Serial.println(" %");
+          Serial.print("Critical value pressure: ");
+          Serial.print(critical_pressure);
+          Serial.println(" mbar");
+          Serial.print("Lower critical temperature value: ");
+          Serial.print(lower_temp_value);
+          Serial.println(" C");
+          Serial.print("Upper critical temperature value: ");
+          Serial.print(upper_temp_value);
+          Serial.println(" C");
+          Serial.println();
 
-              Serial.println("Options:");
-              Serial.println("h <value> - set critical value humidity.");
-              Serial.println("p <value> - set critical value pressure.");
-              Serial.println("tl <value> - set lower critical temperature value.");
-              Serial.println("th <value> - set upper critical temperature value.");
-              Serial.println("reset - device reset.");
-              Serial.println("end - exit from the configuration window.");
-            
+          Serial.println("Options:");
+          Serial.println("h <value> - set critical value humidity.");
+          Serial.println("p <value> - set critical value pressure.");
+          Serial.println("tl <value> - set lower critical temperature value.");
+          Serial.println("th <value> - set upper critical temperature value.");
+          Serial.println("reset - device reset.");
+          Serial.println("start - do the measurement.");   
       }
-      else if (input.equals("end")) 
+
+      if (input.equals("start")) 
       {
         Serial.println();
         Serial.println("Starting sensor data display...");
@@ -171,7 +191,7 @@ void Title_Window()
         {
             Serial.println("Resetting device...");
             delay(5000);
-            //asm volatile ("  jmp 0");
+            resetFunc();
         }
         else 
         {
